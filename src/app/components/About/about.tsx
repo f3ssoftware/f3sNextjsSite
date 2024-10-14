2a3ff303c712b460921282e65fe264769d1e01a4
Fernando Carvalho <nando_carvalhoo@hotmail.com>
2024-10-14 18:42:28 -0300

    responsiveness fully done

--- src/app/components/About/about.tsx
+++ src/app/components/About/about.tsx
@@ -11,7 +11,7 @@ export default function About() {
             padding: '1%'
         }}>
             <BoxTopic title="Software Development" description="We are dedicated to creating exceptional tools and applications tailored to the unique needs of specific business." backgroundImage="/img/imac_vscode.png"></BoxTopic>
-            <BoxTopic title="Community Support" description="Our focus goes beyond development—we actively collaborate with the tech community to enhance frameworks and push the boundaries of technology." backgroundImage="/img/auditorium.png"></BoxTopic>
+            <BoxTopic title="Community Support" description="Our focus goes beyond development — we actively collaborate with the tech community to enhance frameworks and push the boundaries of technology." backgroundImage="/img/auditorium.png"></BoxTopic>
             <BoxTopic title="Games" description="Building technologies and infrastructure into the Game scenario" backgroundImage="/img/videogame.png"></BoxTopic>
             <BoxTopic title="Networking" description="We connect people to develop revolutionary ideas" backgroundImage="/img/meeting.png"></BoxTopic>
         </div>
